c7f5d5b9171e2585a70ee51404e6fae78a3a7b91
Bereket Teshome <bereket.teshome@wikimedia.de>
2020-12-09 19:07:49 +0100

    Implemented async API calls

--- checkConstraints.py
+++ checkConstraints.py
@@ -1,5 +1,7 @@
-import requests
+import asyncio
+from aiohttp import ClientSession
 import csv
+import json
 
 OUTPUT_DELIMITER = ';'
 STATEMENT_COUNT_URL = 'https://www.wikidata.org/w/api.php?action=query&prop=pageprops&ppprop=wb-claims&format=json'
@@ -22,47 +24,53 @@ def printResults(q_id, statementCount, constraintChecks):
         violated_statements]
     )))
 
-def countStatements(q_id):
-    statementCountResponse = requests.get(STATEMENT_COUNT_URL + '&titles=' + q_id).json()
-    pages = statementCountResponse['query']['pages']
-    firstPageId = next(iter(pages))
-    return pages[firstPageId]['pageprops']['wb-claims']
+async def countStatements(q_id):
+    async with ClientSession() as session:
+        async with session.get(STATEMENT_COUNT_URL + '&titles=' + q_id) as statementCountResponse:
+            statementCountResponse = await statementCountResponse.read()
+            r = json.loads(str(statementCountResponse, 'utf-8'))
+            pages = r['query']['pages']
+            firstPageId = next(iter(pages))
+            return pages[firstPageId]['pageprops']['wb-claims']
 
-def checkConstraints(q_id):
+async def checkConstraints(q_id):
     counter = {
         'violations': 0,
         'warnings': 0,
         'suggestions': 0
     }
 
-    r = requests.get(CONSTRAINT_CHECK_URL + '&id=' + q_id).json()
+    async with ClientSession() as session:
+        async with session.get(CONSTRAINT_CHECK_URL + '&id=' + q_id) as r:
+            r = await r.read()
+            parsed_response = json.loads(str(r, 'utf-8'))
 
-    for (property_id, constraint_checks) in r['wbcheckconstraints'][q_id]['claims'].items():
-            for constraint_check in constraint_checks:
-                main_results = constraint_check['mainsnak']['results']
-                for main_result in main_results:
-                    # print("property_id, status:", property_id, main_result['status'])
-                    counter = incrementCounter(main_result['status'], counter)
-                if 'qualifiers' in constraint_check.keys():
-                    qualifier_items = constraint_check['qualifiers'].items()
-                    for (qualifier_property_id, qualifier_item) in qualifier_items:
-                        for qualifier_constraint_check in qualifier_item:
-                            qualifier_results = qualifier_constraint_check['results']
-                            for qualifier_result in qualifier_results:
-                                # print("property_id, qualifier_property_id, status:", property_id, qualifier_property_id, qualifier_result['status'])
-                                counter = incrementCounter(qualifier_result['status'], counter)
+            for (property_id, constraint_checks) in parsed_response['wbcheckconstraints'][q_id]['claims'].items():
+                for constraint_check in constraint_checks:
+                    main_results = constraint_check['mainsnak']['results']
+                    for main_result in main_results:
+                        # print("property_id, status:", property_id, main_result['status'])
+                        counter = incrementCounter(main_result['status'], counter)
+                    if 'qualifiers' in constraint_check.keys():
+                        qualifier_items = constraint_check['qualifiers'].items()
+                        for (qualifier_property_id, qualifier_item) in qualifier_items:
+                            for qualifier_constraint_check in qualifier_item:
+                                qualifier_results = qualifier_constraint_check['results']
+                                for qualifier_result in qualifier_results:
+                                    # print("property_id, qualifier_property_id, status:", property_id, qualifier_property_id, qualifier_result['status'])
+                                    counter = incrementCounter(qualifier_result['status'], counter)
 
-                if 'references' in constraint_check.keys():
-                    reference_items = constraint_check['references']
-                    for reference_item in reference_items:
-                        for (snak_property_id, reference_constraint_checks) in reference_item['snaks'].items():
-                            for reference_constraint_check in reference_constraint_checks:
-                                reference_results = reference_constraint_check['results']
-                                for reference_result in reference_results:
-                                    # print("property_id, snak_property_id, status:", property_id, snak_property_id, reference_result['status'])
-                                    counter = incrementCounter(reference_result['status'], counter)
+                    if 'references' in constraint_check.keys():
+                        reference_items = constraint_check['references']
+                        for reference_item in reference_items:
+                            for (snak_property_id, reference_constraint_checks) in reference_item['snaks'].items():
+                                for reference_constraint_check in reference_constraint_checks:
+                                    reference_results = reference_constraint_check['results']
+                                    for reference_result in reference_results:
+                                        # print("property_id, snak_property_id, status:", property_id, snak_property_id, reference_result['status'])
+                                        counter = incrementCounter(reference_result['status'], counter)
 
-    return counter
+            return counter
 
 
 def incrementCounter(status, counter):
@@ -75,15 +83,18 @@ def incrementCounter(status, counter):
 
     return counter
 
+async def main():
+    printHeader()
+
+    with open('data/input.csv', newline='') as inputFile:
+        lines = list(csv.reader(inputFile))
+
+    for fields in lines:
+        q_id=fields[0]
+        statementCount = await countStatements(q_id)
+        constraintChecks = await checkConstraints(q_id)
+        printResults(q_id, statementCount, constraintChecks)
 
 
-printHeader()
-
-with open('data/input.csv', newline='') as inputFile:
-    lines = list(csv.reader(inputFile))
-
-for fields in lines:
-    q_id=fields[0]
-    statementCount = countStatements(q_id)
-    constraintChecks = checkConstraints(q_id)
-    printResults(q_id, statementCount, constraintChecks)
+loop=asyncio.get_event_loop()
+loop.run_until_complete(main())
