1791f68617d4cadc7ba5361ae949b159028534a1
Bereket teshome <bereket.teshome@wikimedia.de>
2020-12-11 14:20:36 +0100

    handle exceptions duting API request

--- checkConstraints.py
+++ checkConstraints.py
@@ -5,6 +5,7 @@ import os
 import getopt
 import asyncio
 from aiohttp import ClientSession
+from aiohttp import ClientError
 import csv
 import json
 from datetime import datetime
@@ -46,11 +47,11 @@ async def countStatements(q_id):
     # entity does not exist or is a redirect.
     async with ClientSession() as session:
         async with session.get(STATEMENT_COUNT_URL + '&titles=' + q_id) as statementCountResponse:
-            statementCountResponse = await statementCountResponse.read()
-            r = json.loads(str(statementCountResponse, 'utf-8'))
-            pages = r['query']['pages']
-            firstPageId = next(iter(pages))
             try:
+                statementCountResponse = await statementCountResponse.read()
+                r = json.loads(str(statementCountResponse, 'utf-8'))
+                pages = r['query']['pages']
+                firstPageId = next(iter(pages))
                 statementCount = pages[firstPageId]['pageprops']['wb-claims']
             except KeyError:
                 return False
@@ -67,37 +68,46 @@ async def checkConstraints(q_id):
 
     async with ClientSession() as session:
         async with session.get(CONSTRAINT_CHECK_URL + '&id=' + q_id) as r:
-            r = await r.read()
-            parsed_response = json.loads(str(r, 'utf-8'))
+            try:
+                r = await r.read()
+                parsed_response = json.loads(str(r, 'utf-8'))
 
-            for (property_id, statement_group) in parsed_response['wbcheckconstraints'][q_id]['claims'].items():
-                for statement in statement_group:
-                    counter['statement_is_violated'] = False
+                claims = parsed_response['wbcheckconstraints'][q_id]['claims']
 
-                    violated_mainsnaks = statement['mainsnak']['results']
-                    for violated_mainsnak in violated_mainsnaks:
-                        # print("property_id, status:", property_id, main_result['status'])
-                        counter = incrementCounter(violated_mainsnak['status'], counter)
+                if len(claims) == 0:
+                    print("item with q_id: " + q_id + "has no statements.")
+                else:
+                    for (property_id, statement_group) in claims.items():
+                        for statement in statement_group:
+                            counter['statement_is_violated'] = False
 
-                    if 'qualifiers' in statement.keys():
-                        qualifier_items = statement['qualifiers'].items()
-                        for (qualifier_property_id, qualifier_item) in qualifier_items:
-                            for qualifier_constraint_check in qualifier_item:
-                                qualifier_results = qualifier_constraint_check['results']
-                                for qualifier_result in qualifier_results:
-                                    # print("property_id, qualifier_property_id, status:", property_id, qualifier_property_id, qualifier_result['status'])
-                                    counter = incrementCounter(qualifier_result['status'], counter)
+                            violated_mainsnaks = statement['mainsnak']['results']
+                            for violated_mainsnak in violated_mainsnaks:
+                                # print("property_id, status:", property_id, main_result['status'])
+                                counter = incrementCounter(violated_mainsnak['status'], counter)
 
-                    if 'references' in statement.keys():
-                        reference_items = statement['references']
-                        for reference_item in reference_items:
-                            for (snak_property_id, reference_constraint_checks) in reference_item['snaks'].items():
-                                for reference_constraint_check in reference_constraint_checks:
-                                    reference_results = reference_constraint_check['results']
-                                    for reference_result in reference_results:
-                                        # print("property_id, snak_property_id, status:", property_id, snak_property_id, reference_result['status'])
-                                        counter = incrementCounter(reference_result['status'], counter)
-            return counter
+                            if 'qualifiers' in statement.keys():
+                                qualifier_items = statement['qualifiers'].items()
+                                for (qualifier_property_id, qualifier_item) in qualifier_items:
+                                    for qualifier_constraint_check in qualifier_item:
+                                        qualifier_results = qualifier_constraint_check['results']
+                                        for qualifier_result in qualifier_results:
+                                            # print("property_id, qualifier_property_id, status:", property_id, qualifier_property_id, qualifier_result['status'])
+                                            counter = incrementCounter(qualifier_result['status'], counter)
+
+                            if 'references' in statement.keys():
+                                reference_items = statement['references']
+                                for reference_item in reference_items:
+                                    for (snak_property_id, reference_constraint_checks) in reference_item['snaks'].items():
+                                        for reference_constraint_check in reference_constraint_checks:
+                                            reference_results = reference_constraint_check['results']
+                                            for reference_result in reference_results:
+                                                # print("property_id, snak_property_id, status:", property_id, snak_property_id, reference_result['status'])
+                                                counter = incrementCounter(reference_result['status'], counter)
+
+                return counter
+            except ClientError:
+                print("item with q_id: " + q_id + "has exceptions")
 
 
 def incrementCounter(status, counter):
